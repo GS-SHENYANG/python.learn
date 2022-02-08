2562e0beeb5425c494ec1757d9537f72bd0cb4c4
dell 9020sff <27382901@qq.com>
2022-02-08 09:39:56 +0800

    main

---
+++ name.py
@@ -0,0 +1,20 @@
+name = "ada lovelace"
+print(name)
+print(name.title())
+# name.title() 是对name这个变量进行title（）这个函数操作，首字母大写
+
+print(name.upper())
+#name.upper() 全部大写
+print(name.lower())
+#name.lower() 全部小写
+
+#合并字符串
+first_name="ada"
+last_name="lovelace"
+full_name=first_name+ " " +last_name
+print(first_name)
+print(last_name)
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
